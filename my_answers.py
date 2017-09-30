f17861a6392ac052f0bdc9df3315b9a96b87dcf3
Vivek Menon <mail@vvkmnn.xyz>
2017-09-30 16:33:59 -0400

    2.7

--- my_answers.py
+++ my_answers.py
@@ -1,48 +1,86 @@
-import numpy as np
-
-from keras.models import Sequential
+from keras.layers import Activation
 from keras.layers import Dense
 from keras.layers import LSTM
-import keras
+from keras.models import Sequential
+import collections
+import numpy as np
+import string
 
 
-# TODO: fill out the function below that transforms the input series 
-# and window-size into a set of input/output pairs for use with our RNN model
-def window_transform_series(series, window_size, step_size=1):
+def window_transform_series(series, window_size):
+    """
+    Transforms the input series 
+    and window-size into a set of input/output pairs for use with our RNN model
+    """
     # containers for input/output pairs
-    X = [series[i:i+window_size] for i in range(0, len(series)- window_size, step_size)]
-    y = [series[i+window_size] for i in range(0, len(series) - window_size, step_size)]
-
-    # reshape each 
-    X = np.asarray(X) 
+    X = []
+    y = []
+    index = 0
+    while window_size < len(series):
+        X.append(series[index:window_size])
+        y.append(series[window_size])
+        window_size += 1
+        index += 1
+    # reshape each
+    X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
-    y.shape = (len(y),1)
+    y.shape = (len(y), 1)
 
-    return X,y
+    return X, y
+
 
 def build_part1_RNN(window_size):
+    """
+    Build an RNN to perform regression on our time series input/output data using Keras
+    with a fixed size of 5 nodes in the LSTM and one single output node with no activation function
+    so that it can output a value matching the time series.
+    """
     model = Sequential()
-    model.add(LSTM(5, input_shape=(window_size,1)))
+    model.add(LSTM(5, input_shape=(window_size, 1)))
     model.add(Dense(1))
     return model
 
 
-### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
+    """
+    Return the text input with only ascii lowercase and the punctuation given below included.
+    This will also exclude numbers, special foreign characters, ), $, % etc.
+    """
+    # use ascii letters
+    ascii_letters = string.ascii_letters
     punctuation = ['!', ',', '.', ':', ';', '?']
-
+    # find all unique characters in the text
+    unique_char = ''.join(set(text))
+    # remove the non-english characters from text
+    for char in unique_char:
+        if char not in ascii_letters and char not in punctuation:
+            text = text.replace(char, ' ')
     return text
 
-### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
+
 def window_transform_text(text, window_size, step_size):
+    """
+    Transform the input text and window-size into a set of input/output pairs for use with our RNN model
+    We move the window by the parameter step size so that we don't have to generate too many pairs.
+    """
     # containers for input/output pairs
     inputs = []
     outputs = []
+    index = 0
+    while index + window_size < len(text):
+        inputs.append(text[index:index + window_size])
+        outputs.append(text[index + window_size])
+        index += step_size
+    return inputs, outputs
 
-    return inputs,outputs
 
-# TODO build the required RNN model: 
-# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    """
+    Build the required RNN model: 
+    a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+    """
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars, activation='softmax'))
+    return model
